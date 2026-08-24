98540017d58dac1b666f97ec3c02e471f8b72ee7
guilherme <guilhermecoelhog74@gmail.com>
2026-08-24 17:17:11 -0300

    feat: começando o js da calculadora de imc

---
+++ PW2/Imc_PesoIdeal/script.js
@@ -0,0 +1,5 @@
+function calculo(operacao){
+    const campo1 = document.getElementById("Altura")
+    const campo2 = document.getElementById("Peso")
+    const campo3 = document.getElementById("")
+}
